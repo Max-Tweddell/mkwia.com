37c725b31a953bd912a6cfaed28377e00941c39f
Milosz Kwiatkowski <m.kiwi2000@gmail.com>
2016-08-17 23:54:06 +0100

    remove old files

--- header.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <meta charset="utf-8">
-    <title><?php echo $title ?></title>
-    <link rel="stylesheet" type="text/css" href="/assets/style.css">
-    <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
-</head>
