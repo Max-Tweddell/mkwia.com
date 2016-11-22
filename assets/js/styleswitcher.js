fee667d6ac39d0d0737e74207b97e7414414020a
Milosz Kwiatkowski <mk@mkwia.com>
2016-11-22 17:50:39 +0000

    changes styles

--- assets/js/styleswitcher.js
+++ assets/js/styleswitcher.js
@@ -2,7 +2,9 @@ function setActiveStyleSheet(title) {
     document.getElementById("stylesheet").setAttribute("href", "/assets/css/" + title + ".css");
     localStorage.stylesheet = title;
 }
-if (typeof localStorage.stylesheet != "undefined") {
-    var title = localStorage.stylesheet;
-    setActiveStyleSheet(title)
+window.onload = function () {
+    if (typeof localStorage.stylesheet != "undefined") {
+        var title = localStorage.stylesheet;
+        setActiveStyleSheet(title)
+    }
 }
